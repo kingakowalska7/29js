5cebf258dba37486619df777a10fcd2a61a2bfa2
kkowalska <kingaannamaria@gmail.com>
2022-11-10 15:04:34 +0100

    zadanie

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let firstNumber = 3;
+console.log(firstNumber);
+
+let secondNumber = 5;
+console.log(secondNumber);
+
+function add(firstNumber, secondNumber) {
+let result = firstNumber + secondNumber;
+
+return result;
+}
+console.log( add(firstNumber, secondNumber) );
